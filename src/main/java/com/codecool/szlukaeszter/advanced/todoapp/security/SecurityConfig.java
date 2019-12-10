7193b7c6404c85954e3339519b2a50c2e000af48
Eszter Szluka <szluka.eszter@gmail.com>
2019-12-10 18:40:23 +0100

    'add AuthenticationManager bean to SecurityConfig'

--- src/main/java/com/codecool/szlukaeszter/advanced/todoapp/security/SecurityConfig.java
+++ src/main/java/com/codecool/szlukaeszter/advanced/todoapp/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.codecool.szlukaeszter.advanced.todoapp.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,8 +18,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
+                .antMatchers("/login").permitAll() // allowed by anyone
                 .antMatchers("/list").permitAll() // allowed by anyone
                 .antMatchers("/todos/*").hasRole("ADMIN") // allowed only when signed in with admin role
                 .anyRequest().denyAll(); // anything else is denied
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }

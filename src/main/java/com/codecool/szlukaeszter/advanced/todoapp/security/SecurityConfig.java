d98675d5d82f131040e0efd1ee647c9585b74030
Eszter Szluka <szluka.eszter@gmail.com>
2019-12-11 17:32:16 +0100

    'add CustomUserDetailService'

--- src/main/java/com/codecool/szlukaeszter/advanced/todoapp/security/SecurityConfig.java
+++ src/main/java/com/codecool/szlukaeszter/advanced/todoapp/security/SecurityConfig.java
@@ -2,14 +2,21 @@ package com.codecool.szlukaeszter.advanced.todoapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -18,10 +25,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/login").permitAll() // allowed by anyone
+                .antMatchers("/auth/signin").permitAll() // allowed by anyone
                 .antMatchers("/list").permitAll() // allowed by anyone
                 .antMatchers("/todos/*").hasRole("ADMIN") // allowed only when signed in with admin role
-                .anyRequest().denyAll(); // anything else is denied
+                .anyRequest().denyAll() // anything else is denied
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean

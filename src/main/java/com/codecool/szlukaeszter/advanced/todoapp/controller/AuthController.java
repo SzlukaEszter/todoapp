e87a173620ea19805663b0ee3786a5cf2c2e170a
Eszter Szluka <szluka.eszter@gmail.com>
2019-12-16 13:42:21 +0100

    'trying to fix SecurityConfig'

--- src/main/java/com/codecool/szlukaeszter/advanced/todoapp/controller/AuthController.java
+++ src/main/java/com/codecool/szlukaeszter/advanced/todoapp/controller/AuthController.java
@@ -10,10 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -31,6 +28,11 @@ public class AuthController {
     public AuthController(AuthenticationManager authenticationManager, JwtTokenServices jwtTokenServices, TodoAppUserRepository users) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenServices = jwtTokenServices;
+    }
+
+    @GetMapping("/signin")
+    public String simpleResponse(){
+        return "Hello";
     }
 
     @PostMapping("/signin")
--- src/main/java/com/codecool/szlukaeszter/advanced/todoapp/security/SecurityConfig.java
+++ src/main/java/com/codecool/szlukaeszter/advanced/todoapp/security/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.codecool.szlukaeszter.advanced.todoapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.net.http.HttpRequest;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -25,12 +28,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/auth/signin").permitAll() // allowed by anyone
-                .antMatchers("/list").permitAll() // allowed by anyone
-                .antMatchers("/todos/*").hasRole("ADMIN") // allowed only when signed in with admin role
-                .anyRequest().denyAll() // anything else is denied
-                .and()
-                .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
+                .antMatchers( "/auth/signin").permitAll() // allowed by anyone
+                .antMatchers(HttpMethod.POST, "/list").permitAll() // allowed by anyone
+                .antMatchers(HttpMethod.DELETE, "/todos/**").hasRole("USER"); // allowed only when signed in with admin role
+                //.anyRequest().denyAll(); // anything else is denied
+                //.and()
+               // .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
